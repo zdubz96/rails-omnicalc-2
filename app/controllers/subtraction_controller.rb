dbd50eae4cda9ba6abda59d5608e55f21f0b3bd2
zdubz96 <zchan@chicagobooth.edu>
2025-04-26 18:18:05 +0000

    V1

--- app/controllers/subtraction_controller.rb
+++ app/controllers/subtraction_controller.rb
@@ -1,14 +1,14 @@
-class subtraction_controller < ApplicationController
+class SubtractionController < ApplicationController
   def show_subtraction_form
     render({ :template => "subtraction_templates/subtraction_form" })
   end
 
-  def subtract_these
+  def subtract
     @first_number = params.fetch("first_num").to_f
-    second_number = params.fetch("second_num").to_f
+    @second_number = params.fetch("second_num").to_f
 
-    @result =  @second_number - @first_number
+    @result =  @first_number-@second_number 
 
-    render({ :template => "subtraction_templates/subtract_results" })
+    render({ :template => "subtraction_templates/subtraction_results" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "addition", :action => "show_addition_form" })
 
-  get("/ad", { :controller => "addition", :action => "show_addition_form" })
-  get("/wizard_add", { :controller => "addition", :action => "add_these" })
+  get("/add", { :controller => "addition", :action => "show_addition_form" })
+  get("/wizard_addition", { :controller => "addition", :action => "add_these" })
 
-  get("/subtract", { :controller => "subtract", :action => "show_sub_form" })
-  get("", { :controller => "subtraction", :action => "subtract" })
+  get("/subtract", { :controller => "subtraction", :action => "show_subtraction_form" })
+  get("/wizard_subtraction", { :controller => "subtraction", :action => "subtract" })
 
-  get("/multiply", { :controller => "multiplication", :action => "multiplication" })
+  get("/multiply", { :controller => "multiplication", :action => "show_multiplication_form" })
   get("/wizard_multiply", { :controller => "multiplication", :action => "multiply" })
 
   get("/divide", { :controller => "division", :action => "show_division_form" })
-  get("/wizard_div", { :controller => "div", :action => "divide_these" })
+  get("/wizard_divide", { :controller => "division", :action => "divide_these" })
 
 end
